6ffe1f9724202ff804b22c9a5ab4f1c4d24bb191
Kicshikxo <vip.game.game6@gmail.com>
2021-03-13 04:42:16 +0300

    update

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ io.on('connection', async function(socket){
 
 				for (let student of data.students){
 					if (!student) continue
-					let currentStudent = null
+					let currentStudent
 					for (let student_ of database){
 						if (student_.name === student)
 							currentStudent = student_
