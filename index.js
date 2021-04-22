eb659eac583ae80a6869585f8618252fddc0f7a0
Kicshikxo <vip.game.game6@gmail.com>
2021-04-22 22:36:20 +0300

    update

--- index.js
+++ index.js
@@ -43,12 +43,9 @@ function sortDates(a,b){
 app.get('/', async function(request, response){
 	database = await collection.find({}, {projection: {_id: 0}})
 	response.render(__dirname + "/static/HTML/duties_old.html", {database: database})
-	// response.render(__dirname + "/static/HTML/duties_old.html")
 })
 
 app.get('/new', async function(request, response){
-	// database = await collection.find({}, { projection: { _id: 0}})
-	// response.render(__dirname + "/public/HTML/duties.html", {database: database})
 	response.render(__dirname + "/static/HTML/duties.html")
 })
 
